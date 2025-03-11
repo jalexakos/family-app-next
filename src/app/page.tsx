19763c944ab7a6d1a0dfbe1f03789f662acb9b42
Josh Alexakos <josiah.alexakos@gmail.com>
2025-03-10 20:22:14 -0700

    Install themes and dropdown menu, and add to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Tracker from "./dog-tracker/tracker";
+import Toggle from "./toggle";
 
 export default function Home() {
   return (
     <main>
+      <Toggle />
       <Tracker />
     </main>
   );
